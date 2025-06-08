2e6a23974a2b14c1299fec0e59bc68ed28ee6799
sorlych <sorlych@gmail.com>
2025-06-08 13:05:35 -0400

    Try and just use df instead of hugging face load dataset

--- bart_first_round_no_sum.py
+++ bart_first_round_no_sum.py
@@ -6,8 +6,8 @@ import os
 import pandas as pd
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 prompt = "Please give a summary of this article"
-
-dataset = load_dataset("csv", data_files="dev.csv", streaming=True)
+dataset = pd.read_csv("dev.csv")
+# dataset = load_dataset("csv", data_files="dev.csv", streaming=True)
 def batched_df(df, batch_size):
     for start in range(0, len(df), batch_size):
         yield df.iloc[start:start + batch_size]
