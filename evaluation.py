a8530f091787cb5898b9a514a9cd988e19315c17
sorlych <sorlych@gmail.com>
2025-06-19 11:59:46 -0400

    evaluted results of no context summary

---
+++ evaluation.py
@@ -0,0 +1,29 @@
+from nltk import meteor, word_tokenize
+from nltk.translate.meteor_score import meteor_score
+import pandas as pd
+import numpy as np
+def calculate_metor():
+    df_dataset = pd.read_csv("dev.csv")
+    df_summaries = pd.read_csv("out_summaries_trimmed.csv")
+    # df_summaries_context
+    df_dataset_summaries = pd.concat([df_dataset, df_summaries], axis=1)
+    print(df_dataset_summaries.head())
+    print(df_dataset_summaries.columns)
+    # df_dataset_summaries_context
+    score = 0
+    for _,row in df_dataset_summaries.iterrows():
+        print(row["normalized claim"],"\n",row["summary"])
+        print("\n\n",row["summary"] )
+        if pd.isna(row["summary"]):
+            print("not a filled row!")
+        else:
+            score += meteor_score([word_tokenize(row["normalized claim"])], word_tokenize(row["summary"]))
+    avg = round(score/1171, 5)
+    print(avg)    
+
+
+def main():
+    calculate_metor()
+if __name__ == "__main__":
+    main()
+
